afa99c383950e6b0f320e945d04032f7ac1fcaf3
Brandon <bvargas40854@ufide.ac.cr>
2024-12-11 18:33:28 -0600

    updTE

--- InitializrSpringbootProject/src/main/java/EjercicioPractico2_BrandonVargasMoreira/demo/Controller/CarritoController.java
+++
@@ -1,5 +0,0 @@
-package main.java.EjercicioPractico2_BrandonVargasMoreira.demo.Controller;
-
-public class CarritoController {
-
-}
--- src/main/java/Essentials/demo/dao/VentaDao.java
+++
@@ -1,8 +0,0 @@
-package Essentials.demo.dao;
-
-import Essentials.demo.domain.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VentaDao extends JpaRepository <Venta,Long> {
-     
-}
